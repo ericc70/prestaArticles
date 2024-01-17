23afea9454b094e86c8fb0edc390f91824187273
ericc70 <eric.c@codeur.online>
2024-01-17 16:48:40 +0100

    methods getCountQueryBuilder && getQueryBuilder

--- Grid/Query/ArticleQueryBuilder.php
+++ Grid/Query/ArticleQueryBuilder.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Ericc70\Openarticles\Grid\Querry;
 
 use Doctrine\DBAL\Connection;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use PrestaShop\PrestaShop\Core\Grid\Query\Filter\SqlFilters;
 use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
 use PrestaShop\PrestaShop\Core\Grid\Query\AbstractDoctrineQueryBuilder;
-use PrestaShop\PrestaShop\Core\Grid\Query\DoctrineSearchCriteriaApplicatorInterface;
 use PrestaShop\PrestaShop\Core\Grid\Query\Filter\DoctrineFilterApplicatorInterface;
+use PrestaShop\PrestaShop\Core\Grid\Query\DoctrineSearchCriteriaApplicatorInterface;
 
 class ArticleQueryBuilder extends AbstractDoctrineQueryBuilder
 {
@@ -45,8 +48,58 @@ class ArticleQueryBuilder extends AbstractDoctrineQueryBuilder
     {
         #
     }
+
+    
     public function getCountQueryBuilder(SearchCriteriaInterface $searchCriteria)
     {
-        #   
+          $db = $this->getQueryBuilder( $searchCriteria->getFilters());
+          $db->select('COUNT(oa.id');
+
+          return $db;
+    }
+
+    private function getQueryBuilder(array $filterValues) :QueryBuilder
+    {
+        $db = $this->connection
+        ->createQueryBuilder()
+        ->from($this->dbPrefix.'open_articles', 'oa')
+        ->leftjoin(
+            'oa',
+            $this->dbPrefix.'open_articles_lang',
+            'oal',
+            'oal.open_article_id = oa.id AND oal.lang_id = : lang_id'
+        ); 
+
+        $sqlFilters = new SqlFilters();
+        $sqlFilters->addFilter(
+            'id',
+            'oa.id',
+            SqlFilters::WHERE_STRICT
+        );
+        $this->filterApplicator->apply($db, $sqlFilters, $filterValues);
+        $db->setParameter('lang_id', $this->contextLangId);
+
+        foreach ($filterValues as $filterName => $filter){
+            if('active' === $filterName){
+                $db->andWhere('oa.active = :active' );
+                $db->setParameter('active', $filter);
+
+                continue;
+            }
+            if('title' === $filterName){
+                $db->andWhere('oa.title = :title' );
+                $db->setParameter('title ','%'. $filter.'%');
+
+                continue;
+            }
+            if('position' === $filterName){
+                $db->andWhere('oa.position = :position' );
+                $db->setParameter('position ','%'. $filter.'%');
+
+                continue;
+            }
+         
+        }
+            return $db;
     }
 }
